3f71c984f9b8f2c4ca9ec89e8b5ead0b966202ab
mguppy <mguppy34@gmail.com>
2021-07-29 15:43:07 -0500

    delete category successful

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,6 +38,9 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where: {id: req.params.id}})
+  .then(data => res.json(data))
+  .catch(err => res.json(err))
 });
 
 module.exports = router;
